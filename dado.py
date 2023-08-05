75f2852ee79360fe812da646c74e4dc2877aa58e
rfilippod <rodrigofilippo@gmail.com>
2023-08-05 15:50:59 -0300

    tratando os dados

--- dado.py
+++ dado.py
@@ -2,16 +2,20 @@ import pandas as pd
 
 
 def read(data):
-    # Use a breakpoint in the code line below to debug your script.
+
     df = pd.read_csv(data, sep=",", encoding='cp1252')
 
-    df['Country Name'].fillna('No data', inplace=True)  # Press Ctrl+F8 to toggle the breakpoint.
-    print(df.head(5).to_string())  # Press Ctrl+F8 to toggle the breakpoint.
+    df['Country Name'].fillna('No data', inplace=True)
+    df['2019 [YR2019]'].fillna('0', inplace=True)
+    df['2019 [YR2019]'] = df['2019 [YR2019]'].str.replace('..','0')
+    dado_int = df['2019 [YR2019]'].astype('float')
+    dado_int = dado_int.astype('int')
+    pd.to_numeric(dado_int)
+    print(dado_int.mean())
+    print(dado_int.head())
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     data = "./archive/0d292e16-2f1b-452b-9c91-4a3f9fed30cc_Data.csv"
     read(data)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
